8e8c834da092b30a59d4273812c1130bcf410865
hzj <1091070904@qq.com>
2019-02-04 22:34:45 +0800

    提交

--- app/src/main/java/com/example/huzj/weather/ChooseAreaFragment.java
+++ app/src/main/java/com/example/huzj/weather/ChooseAreaFragment.java
@@ -2,6 +2,7 @@ package com.example.huzj.weather;
 
 import android.app.Fragment;
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -75,6 +76,13 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel==LEVEL_CITY){
                     selectedCity=cityList.get(position);
                     queryCounties();
+                }else if (currentLevel==LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    System.out.println("11111111111111==="+weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
@@ -124,7 +132,7 @@ public class ChooseAreaFragment extends Fragment {
                     getActivity().runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
-
+                            closeProgressDialog();
                             if ("province".equals(type)){
                                 queryProvinces();
                             }else if ("city".equals(type)){
@@ -196,7 +204,7 @@ public class ChooseAreaFragment extends Fragment {
 
     private void queryCounties(){
         titleText.setText(selectedCity.getCityName());
-        backButton.setText(View.VISIBLE);
+        backButton.setVisibility(View.VISIBLE);
         countyList=DataSupport.where("cityid = ?",String.valueOf(selectedCity.getId())).find(County.class);
         if (countyList.size()>0){
             dataList.clear();
